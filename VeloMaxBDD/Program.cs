5b13becad7c81117d13c3296ee1d28855de11df1
Valentin AYMONIER <valentin.aymonier@edu.devinci.fr>
2021-05-12 13:42:17 +0200

    Test

--- VeloMaxBDD/Program.cs
+++ VeloMaxBDD/Program.cs
@@ -11,6 +11,8 @@ namespace VeloMaxBDD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            
+            Console.WriteLine("Hello World Test");
             Console.ReadKey();
         }
     }
