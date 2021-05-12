02b5316c2762bba239b7749ca82724222ee2e35a
LouisB <louis.badr@edu.devinci.fr>
2021-05-12 15:40:59 +0200

    modif des sets de piece

--- VeloMaxBDD/Piece.cs
+++ VeloMaxBDD/Piece.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.IO;
 
 namespace VeloMaxBDD
@@ -28,47 +27,47 @@ namespace VeloMaxBDD
         public int No_piece
         {
             get { return no_piece; }
+            set
+            {
+                no_piece = value;
+                Connexion.doSQL("update piece set no_piece=" + value + " where no_piece=" + no_piece);
+            }
         }
         public string Desc_piece
         {
             get { return desc_piece; }
+            set
+            {
+                desc_piece = value;
+                Connexion.doSQL("update piece set desc_piece=" + value + " where no_piece=" +no_piece+";");
+            }
         }
         public string Date_intro_piece
         {
             get { return date_intro_piece; }
+            set
+            {
+                date_intro_piece = value;
+                Connexion.doSQL("update piece set date_intro_piece=" + value + " where no_piece=" + no_piece + ";");
+            }
         }
         public string Date_disco_piece
         {
             get { return date_disco_piece; }
+            set
+            {
+                date_disco_piece = value;
+                Connexion.doSQL("update piece set date_disco_piece=" + value + " where no_piece=" + no_piece + ";");
+            }
         }
         public int Stock
         {
             get { return stock; }
-        }
-        public void set_no_piece()
-        {
-
-        }
-        public void set_desc_piece(MySqlConnection connection,string new_desc_piece)
-        {
-            connection.Open();
-            MySqlCommand command = connection.CreateCommand();
-            command.CommandText = $"update piece set desc_piece = \"{new_desc_piece}\" where no_piece = \"{this.no_piece}\";";
-            MySqlDataReader reader;
-            reader = command.ExecuteReader();
-            this.desc_piece = new_desc_piece;
-        }
-        public void set_date_intro_piece()
-        {
-
-        }
-        public void set_date_disco_piece()
-        {
-
-        }
-        public void set_stock()
-        {
-
+            set
+            {
+                stock = value;
+                Connexion.doSQL("update piece stock=" + value + " where no_piece=" + no_piece + ";");
+            }
         }
     }
 }
