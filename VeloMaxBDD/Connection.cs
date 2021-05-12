57f1f82451649df0b8a20b2bb0187d5c0b460dda
Valentin AYMONIER <valentin.aymonier@edu.devinci.fr>
2021-05-12 15:46:52 +0200

    pas besoin ducoup?

---
+++ VeloMaxBDD/Connection.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace VeloMaxBDD
+{
+    class Connection
+    {
+
+        public static string connectionString = "SERVER=localhost;PORT=3306;" +
+                                         "DATABASE=veloMax;" +
+                                         "UID=root;PASSWORD=Valou1234";
+        public static void SQLcomm (string mySQLCommand)
+        {
+            MySqlConnection connection = new MySqlConnection(Connection.connectionString);
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = mySQLCommand;
+            MySqlDataReader reader;
+            reader = command.ExecuteReader();
+            
+        }
+    }
+}
