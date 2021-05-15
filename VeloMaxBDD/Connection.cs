23c4bc22556c200f8f614f82a7b115e48989c946
Valentin AYMONIER <valentin.aymonier@edu.devinci.fr>
2021-05-15 12:17:01 +0200

    ,

--- VeloMaxBDD/Connection.cs
+++ VeloMaxBDD/Connection.cs
@@ -9,7 +9,7 @@ namespace VeloMaxBDD
         public static string connectionString = "SERVER=localhost;PORT=3306;" +
                                          "DATABASE=veloMax;" +
                                          "UID=root;PASSWORD=Valou1234";
-        public static void SQLcomm (string mySQLCommand)
+        public static void update (string mySQLCommand)
         {
             MySqlConnection connection = new MySqlConnection(Connection.connectionString);
             connection.Open();
@@ -17,7 +17,31 @@ namespace VeloMaxBDD
             command.CommandText = mySQLCommand;
             MySqlDataReader reader;
             reader = command.ExecuteReader();
-            
+            connection.Close();
+        }
+        public static void select (string mySQLcommand)
+        {
+            MySqlConnection connection = new MySqlConnection(Connection.connectionString);
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = mySQLcommand;
+            MySqlDataReader reader;
+            reader = command.ExecuteReader();
+            string[] valueString = new string[reader.FieldCount];
+            while (reader.Read())
+            {
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    valueString[i] = reader.GetValue(i).ToString();
+                    Console.Write(valueString[i] + " , ");
+                }
+                Console.WriteLine();
+            }
+            reader.Close();
+            command.Dispose();
+            connection.Close();
+
         }
     }
 }
